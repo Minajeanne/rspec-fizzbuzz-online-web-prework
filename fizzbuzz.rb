ee09b02e3b8682123a8b67e6f0c46451498d0d27
Alicia Santiago <albrown07@mac.com>
2019-02-07 19:17:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@
 def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
-  elsif number % 5
+  elsif number % 5 == 0
     "Buzz"
-  elsif number % 3 == 0
-    "Fizz"
+  elsif number % 3 && number % 5 == 0
+    "FizzBuzz"
   elsif number !% 5 && number % 3
     "Buzz"
   else
