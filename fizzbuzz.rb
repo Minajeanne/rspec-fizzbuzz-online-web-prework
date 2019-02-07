da961e94f26ded18babf38dfc5a04855178932f7
Alicia Santiago <albrown07@mac.com>
2019-02-07 19:16:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
 def fizzbuzz(number)
-  if 
+  if number % 3 == 0
+    "Fizz"
+  elsif number % 5
+    "Buzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number !% 5 == 0
+    "Buzz"
+  else
+    number
+  end
 end
